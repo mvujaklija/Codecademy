69678ebf8b14f2f79b00ade046e7defe339b1920
RIDDLER <milanv@sbb.rs>
2023-01-04 21:33:31 +0100

    Delete HelloYuo.java

--- Java Introductions and Variables/src/HelloYuo.java
+++
@@ -1,7 +0,0 @@
-public class HelloYuo {
-
-    public static void main(String[] args) {
-
-        System.out.println("Hello Milan! A wonderful day to start JAVA lessons from scratch :)");
-    }
-}
