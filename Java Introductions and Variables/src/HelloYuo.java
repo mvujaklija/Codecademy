daee02e8a525e0c6c2c8cb5d6098435956415339
RIDDLER <milanv@sbb.rs>
2023-01-04 21:29:50 +0100

    First files

---
+++ Java Introductions and Variables/src/HelloYuo.java
@@ -0,0 +1,7 @@
+public class HelloYuo {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello Milan! A wonderful day to start JAVA lessons from scratch :)");
+    }
+}
